ae5bcd5a6c24fe575686566938905052781176cd
chaeyoon1 <dornsms777@naver.com>
2017-03-24 20:49:01 +0900

    Add files via upload
    3/20일 실습, 기존의 것 리뉴얼

---
+++ CheckingAccount.java
@@ -0,0 +1,39 @@
+package lecture9;
+
+public class CheckingAccount extends Account
+{
+	private double credit_limit;
+	private double interest;
+	private double loan_interest;
+							
+	public CheckingAccount (double interest, double loan_interest)
+	{
+		credit_limit = 0.00;
+		this.interest = interest;
+		this.loan_interest = loan_interest;
+	}
+	
+	
+	public void debit ( double debitbalance)
+	{
+		if( debitbalance >= credit_limit)
+		{	
+			balance = balance - debitbalance;
+		}	
+		else
+		{
+			balance = balance - debitbalance;
+			System.out.print("Debit amount exceeded account balance.\n");
+		}
+	}
+	
+	public void nextMonth()
+	{
+		if(balance>=0)
+			balance = balance*(1+interest);
+		
+		else 
+			balance = balance*(1+loan_interest);
+		
+	}
+}
