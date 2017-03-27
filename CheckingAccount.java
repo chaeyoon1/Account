c3924940e4cd1270eee78924a5224fcf4db67c54
chaeyoon1 <dornsms777@naver.com>
2017-03-27 13:28:39 +0900

    Add files via upload
    체킹어카운트 마지막수정

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -2,38 +2,43 @@ package lecture9;
 
 public class CheckingAccount extends Account
 {
-	private double credit_limit;
-	private double interest;
-	private double loan_interest;
-							
-	public CheckingAccount (double interest, double loan_interest)
-	{
-		credit_limit = 0.00;
-		this.interest = interest;
-		this.loan_interest = loan_interest;
-	}
-	
-	
-	public void debit ( double debitbalance)
-	{
-		if( debitbalance >= credit_limit)
-		{	
-			balance = balance - debitbalance;
-		}	
-		else
-		{
-			balance = balance - debitbalance;
-			System.out.print("Debit amount exceeded account balance.\n");
-		}
-	}
-	
-	public void nextMonth()
-	{
-		if(balance>=0)
-			balance = balance*(1+interest);
-		
-		else 
-			balance = balance*(1+loan_interest);
-		
-	}
+   private double credit_limit;
+   private double interest;
+   private double loan_interest;
+                     
+   public CheckingAccount (double interest, double loan_interest)
+   {
+      credit_limit = 500.00;
+      this.interest = interest;
+      this.loan_interest = loan_interest;
+   }
+   
+   
+   public void debit ( double debitbalance)
+   {
+      if( getbalance() - debitbalance >= 0.00)
+      {   
+         setBalance(getbalance() - debitbalance);
+      }   
+      else if( debitbalance >credit_limit)
+      {
+    	  System.out.print("\nDebit amount exceeded credit limit!");
+      }
+      
+      else
+      {
+    	  System.out.print("\nDebit amount exceeded account balance.");
+    	  setBalance(getbalance() - debitbalance);
+      }
+   }
+   
+   public void nextMonth()
+   {
+      if(getbalance()>=0)
+         setBalance(getbalance()*(1+interest));
+      
+      else 
+         setBalance((getbalance()*(1+loan_interest)));
+      
+   }
 }
